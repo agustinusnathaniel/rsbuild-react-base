4c355fcbeffaf1ed7e6b9b9e66512ed7d84ef41b
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-01-06 11:00:20 +0700

    chore: install million
    https://million.dev/docs/install#add-the-compiler-to-your-application
    https://rsbuild.dev/config/plugins#rspack-plugins

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,9 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from '@rsbuild/core';
 import { pluginAssetsRetry } from '@rsbuild/plugin-assets-retry';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
+import million from 'million/compiler';
 
 export default defineConfig({
   plugins: [
@@ -14,5 +14,10 @@ export default defineConfig({
   ],
   html: {
     template: './public/index.html',
+  },
+  tools: {
+    rspack: {
+      plugins: [million.webpack({ auto: true })],
+    },
   },
 });
